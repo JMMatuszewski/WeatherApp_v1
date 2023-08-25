73c91dfbcb4ac7c17e8113870c48702cf03f05c8
JMMatuszewski <jmm.matuszewski.programista@gmail.com>
2023-08-25 19:09:06 +0200

    working tree done

--- src/main/java/pl/example/weatherapp_v1/Launcher.java
+++ src/main/java/pl/example/weatherapp_v1/Launcher.java
@@ -2,6 +2,7 @@ package pl.example.weatherapp_v1;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,8 +11,12 @@ import java.io.IOException;
 public class Launcher extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("MainWindow.fxml"));//getClass().getResource("/MainWindow.fxml"
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        Parent parent = FXMLLoader.load(Launcher.class.getResource("MainWindow.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("MainWindow.fxml"));//getClass().getResource("/MainWindow.fxml"
+        //Scene scene = new Scene(fxmlLoader.load(), 600, 300);
+
+        Scene scene = new Scene(parent, 600, 300);
         stage.setTitle("Main Window");
         stage.setScene(scene);
         stage.show();
